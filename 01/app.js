7c6eb10e0b6611933bd22bc6086c16a0c408e2b6
Jacek Zielinski <gandi34@wp.pl>
2022-09-11 13:00:43 +0200

    01 array basics

---
+++ 01/app.js
@@ -0,0 +1,10 @@
+const users = ["Jacek Zielinski",'Zuzanna Zielinska','Julia Zielinska','Malwina Zielinska','Piotr Zielinski','Elżbieta Zielinska','Monika Zielinska','Tomasz Zielinski',"Stanisław Zielinski",'Zdzisław Zielinski'];
+
+console.log(users);
+
+console.log(users.length + ' ' + "elementów zawiera tablica");
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+
